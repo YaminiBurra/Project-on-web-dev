d50ab770538fead8ebee174f9feea68f8a142e8e
YaminiBurra <yaminisb03@gmail.com>
2024-05-09 20:27:40 +0530

    container config done

---
+++ javascript.js
@@ -0,0 +1,28 @@
+        let a="33abc";
+        let b=Number(a)
+        console.log(b)
+        console.log(typeof b)//number is the output
+//1=> true(boolean)
+//0=>false
+//""=> false
+//"fes"=>true; //undefined=>NaN ;  
+//null=>standalone value(empty)
+//undefined => value is not defined
+//symbols=>unique
+//object
+console.log(typeof a)
+//Functions:conversions and operations
+let score="33kok"//gives value as nan
+let val=Number(score)
+console.log(val);
+//"33"=>number
+//"abd44"=>NaN
+//m=true=>1
+let x=1
+let valu=Boolean(x)
+console.log(valu)
+
+
+
+
+
